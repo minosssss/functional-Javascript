a04e6fdde642941e0050df2e7b5f9a8cd059d16d
minosssss <mh.lee239@gmail.com>
2022-05-01 16:23:00 +0900

    함수를 값으로 다루기

--- 05. 코드를 값으로 다루어 표현력 높이기/js/fx.js
+++ 05. 코드를 값으로 다루어 표현력 높이기/js/fx.js
@@ -1,23 +1,25 @@
 const log = console.log;
 
+const curry = f => 
+(a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const map = (f, iter) => {
+const map = curry((f, iter) => {
     let res = [];
     for (const a of iter){
         res.push(f(a))
     }
     return res;
-}
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
     let res = [];
     for (const a of iter) {
         if (f(a)) res.push(a);
     }
     return res;
-}
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
     if (!iter) {
         iter = acc[Symbol.iterator]();
         acc = iter.next().value;
@@ -27,4 +29,5 @@ const reduce = (f, acc, iter) => {
 
     }
     return acc;
-}
+
+});
